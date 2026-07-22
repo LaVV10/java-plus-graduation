c93c11c681d608b5f21540cebe4756b88ff88827
LaVV10 <vit.laz2@mail.ru>
2026-07-22 11:07:58 +0300

    Update AggregatorApplication.java

--- aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/ru/practicum/aggregator/AggregatorApplication.java
@@ -4,18 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-/**
- * Сервис Aggregator (Этап 3-2).
- *
- * Потоково читает {@link ru.practicum.ewm.stats.avro.UserActionAvro} из Kafka-топика
- * {@code stats.user-actions.v1}, инкрементально пересчитывает косинусное сходство мероприятий
- * (см. {@link ru.practicum.aggregator.similarity.SimilarityCalculator}) и отправляет
- * {@link ru.practicum.ewm.stats.avro.EventSimilarityAvro} в топик
- * {@code stats.events-similarity.v1}.
- *
- * Реализован на Kafka Streams с тремя state stores (cм. {@code AggregatorStreamsConfig}).
- * Регистрируется в Eureka как {@code aggregator}; конфигурацию тянет из Config Server.
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AggregatorApplication {
